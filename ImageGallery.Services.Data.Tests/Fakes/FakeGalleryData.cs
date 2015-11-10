1610cc8457a10791de4a3b5b5cb21d8e4b94847e
juvemar <martoo.atanasov@gmail.com>
2015-11-10 19:22:09 +0200

    Unit tests for the TagService are done.

--- ImageGallery.Services.Data.Tests/Fakes/FakeGalleryData.cs
+++ ImageGallery.Services.Data.Tests/Fakes/FakeGalleryData.cs
@@ -8,6 +8,7 @@
 
     public class FakeGalleryData : IImageGalleryData
     {
+        private FakeRepository<Tag> tags;
         private FakeRepository<Album> albums;
         private FakeRepository<User> users;
 
@@ -44,7 +45,12 @@
         {
             get
             {
-                throw new NotImplementedException();
+                if (this.tags == null)
+                {
+                    this.tags = new FakeRepository<Tag>();
+                }
+
+                return this.tags;
             }
         }
 
