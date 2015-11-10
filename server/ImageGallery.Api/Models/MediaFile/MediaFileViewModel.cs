09d954df223781ce1e395bf7b03de9a887537de9
Maskln <lyubomir.nanev@gmail.com>
2015-11-10 18:54:10 +0200

    Added MediaFile Model

---
+++ server/ImageGallery.Api/Models/MediaFile/MediaFileViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ImageGallery.Api.Models.MediaFile
+{
+    using ImageGallery.Api.Infrastructure.Mappings;
+    using ImageGallery.Models;
+
+    public class MediaFileViewModel : IMapFrom<MediaFile>
+    {
+        public int Id { get; set; }
+
+        public string UrlPath { get; set; }
+    }
+}
