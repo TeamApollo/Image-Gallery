9176118a85436cc775e69e84db5e8c6bafc4f620
juvemar <martoo.atanasov@gmail.com>
2015-11-18 12:17:43 +0200

    Made some changes to comment controllers and models

--- Services/ImageGallery.Services.Data/CommentsService.cs
+++ Services/ImageGallery.Services.Data/CommentsService.cs
@@ -36,6 +36,11 @@
                 .All()
                 .FirstOrDefault(u => u.UserName == username);
 
+            if (currentUser == null)
+            {
+                throw new ArgumentNullException("There is no user with such username.");
+            }
+
             comment.Author = currentUser;
 
             this.data.Comments.Add(comment);
@@ -48,6 +53,7 @@
         /// Deletes the comment with the provided id.
         /// </summary>
         /// <param name="id">The id of the comment to delete.</param>
+        /// <param name="authorizedUser">The username of the user whos comment to delete.</param>
         /// <returns>The id of the deleted comment or -1 if no item with such id is found.</returns>
         public int DeleteCommentById(int id, string authorizedUser)
         {
@@ -60,12 +66,16 @@
                 return -1;
             }
 
+            this.data.Comments.Delete(commentToDelete);
+            this.data.SaveChanges();
+
             return commentToDelete.Id;
         }
 
         /// <summary>
         /// Gets all comments.
         /// </summary>
+        /// <param name="username">The username of the user whos comments to get.</param>
         /// <returns>All found comments.</returns>
         public IQueryable<Comment> GetAll(string username)
         {
@@ -80,6 +90,7 @@
         /// Gets the comment with the provided id.
         /// </summary>
         /// <param name="id">The id of the comment to get.</param>
+        /// <param name="username">The username of the user whos comment to get.</param>
         /// <returns>Found comment or null if not found.</returns>
         public IQueryable<Comment> GetById(int id, string username)
         {
