2495fc8b6d3bade148510f9072ac2287699143ed
kossov <ogi_2112@yahoo.com>
2015-11-19 21:52:23 +0200

    images can now be uploaded and viewed
    to view an image, open an album

--- Client/Scripts/Controllers/media-file-controller.js
+++ Client/Scripts/Controllers/media-file-controller.js
@@ -30,32 +30,36 @@ var mediaFileController = (function () {
     }
 
     function uploadFile(albumId) {
-        var url = defaultRoute + 'api/file-upload',
+        var url = defaultRoute + 'api/fileupload',
             data = new FormData(),
+            headers = {
+                "X-Album-Id": albumId,
+                'Authorization': 'Bearer ' + localStorage.getItem(ACCESSTOKEN)
+            },
             files = $("#up-alb-media").get(0).files; // input field
 
         if (files.length > 0) {
             data.append("UploadedImage", files[0]);
         }
 
-        var options = {
-            data: data,
-            headers: {
-                "X-Album-Id": albumId,
-                'Authorization': 'Bearer ' + localStorage.getItem(ACCESSTOKEN)
-            }
-        };
-
         var promise = new Promise(function (res, rej) {
-            requester.post(url, options)
-                .then(function (result) {
-                    toastr.success('Uploaded successfully!');
-                    res();
-                })
-                .catch(function (err) {
-                    toastr.error("Couldn't upload!");
-                    rej();
-                });
+            $.ajax({
+                type: "POST",
+                url: "http://localhost:55833/api/fileupload",
+                contentType: false,
+                processData: false,
+                data: data,
+                headers: headers,
+                success: function(result) {
+                    toastr.success("Media file uploaded successfully!");
+                    res(result);
+                },
+                error: function(error) {
+                    console.log(error);
+                    toastr.error("Error uploading media file!");
+                    rej(error);
+                }
+            });
         });
 
         return promise;
