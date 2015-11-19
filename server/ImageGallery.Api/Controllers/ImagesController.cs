a2a9ebbefd1428da048da424ad197164edfd2fcd
SimoPrG <simeon_petev@hotmail.com>
2015-11-19 17:13:31 +0200

    Add ImageController GET method per album

--- server/ImageGallery.Api/Controllers/ImagesController.cs
+++ server/ImageGallery.Api/Controllers/ImagesController.cs
@@ -1,36 +1,44 @@
 ﻿namespace ImageGallery.Api.Controllers
 {
+    using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Web.Http;
     using System.Web.Http.Cors;
     using AutoMapper.QueryableExtensions;
-    using Models.MediaFile;
+    using Models.Image;
     using Services.Data.Contracts;
     using ImageGallery.Models;
     using AutoMapper;
+    using Common;
 
     [EnableCors("*", "*", "*")]
-    public class MediaFilesController : ApiController
+    public class ImagesController : ApiController
     {
-        private readonly IMediaFilesService mediaFilesService;
+        private readonly IImagesService imagesService;
 
-        public MediaFilesController(IMediaFilesService mediaFilesService)
+        public ImagesController(IImagesService imagesService)
         {
-            this.mediaFilesService = mediaFilesService;
+            this.imagesService = imagesService;
         }
 
-        // GET api/albums/{albumId}/mediafiles
-        [Route("api/albums/{albumId}/mediafiles")]
+        // GET api/images?albumId=XXX
         public IHttpActionResult Get(int albumId)
         {
             string currentUserName = this.User.Identity.Name;
 
-            var result = this.mediaFilesService
-                .GetAll(albumId, currentUserName)
-                .ProjectTo<MediaFileViewModel>()
-                .ToList();
-
-            if (result == null)
+            List<ImageViewModel> result;
+            try
+            {
+                result = this.imagesService
+                    .GetAll(albumId, currentUserName)
+                    .ProjectTo<ImageViewModel>().ToList();
+            }
+            catch (ImageGalleryException)
+            {
+                return this.Unauthorized();
+            }
+            catch(ArgumentNullException)
             {
                 return this.NotFound();
             }
@@ -38,15 +46,14 @@
             return this.Ok(result);
         }
 
-        // GET api/albums/{albumId}/mediafiles/{mediaFileId}
-        [Route("api/albums/{albumId}/mediafiles/{mediaFileId}")]
-        public IHttpActionResult Get(int albumId, int mediaFileId)
+        // GET api/images?album=XXX&imageId=XXX
+        public IHttpActionResult Get(int albumId, int imageId)
         {
             string currentUserName = this.User.Identity.Name;
 
-            var result = this.mediaFilesService
-                .GetById(albumId, mediaFileId, currentUserName)
-                .ProjectTo<MediaFileViewModel>()
+            var result = this.imagesService
+                .GetById(albumId, imageId, currentUserName)
+                .ProjectTo<ImageViewModel>()
                 .FirstOrDefault();
 
             if (result == null)
@@ -57,10 +64,9 @@
             return this.Ok(result);
         }
 
-        // POST api/albums/{albumId}/mediafiles
+        // POST api/images/{albumId}
         [Authorize]
-        [Route("api/albums/{albumId}/mediafiles")]
-        public IHttpActionResult Post(int albumId, MediaFileBindingModel mediaFileRequestModel)
+        public IHttpActionResult Post(int albumId, ImageBindingModel mediaFileRequestModel)
         {
             if (!this.ModelState.IsValid)
             {
@@ -70,20 +76,19 @@
             var currentUser = this.User.Identity.Name;
             var newMediaFile = Mapper.Map<Image>(mediaFileRequestModel);
 
-            var createdProjectId = this.mediaFilesService
+            var createdProjectId = this.imagesService
                 .Add(newMediaFile, albumId, currentUser);
 
             return this.Ok(createdProjectId);
         }
 
-        // DELETE api/albums/{albumId}/mediaFiles/{mediaFileId}
+        // DELETE api/images/{albumId}/{imageId}
         [Authorize]
-        [Route("api/albums/{albumId}/mediafiles/{mediaFileId}")]
         public IHttpActionResult Delete(int albumId, int mediaFileId)
         {
             var currentUser = this.User.Identity.Name;
 
-            int deletedId = this.mediaFilesService.Delete(mediaFileId, albumId, currentUser);
+            int deletedId = this.imagesService.Delete(mediaFileId, albumId, currentUser);
 
             if (deletedId < 0)
             {
