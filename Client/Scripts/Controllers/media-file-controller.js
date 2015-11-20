74de3455d39d2bbdf5f9a389755e6055b6248327
kossov <ogi_2112@yahoo.com>
2015-11-20 12:53:09 +0200

    small fix - defaultRoute

--- Client/Scripts/Controllers/media-file-controller.js
+++ Client/Scripts/Controllers/media-file-controller.js
@@ -1,5 +1,5 @@
 var mediaFileController = (function () {
-    var defaultRoute = 'http://localhost:55833/',
+    var defaultRoute = 'http://imagegallery2015.azurewebsites.net/',
         ACCESSTOKEN = 'x-auth-token',
         USERNAME = 'x-user-name';
 
